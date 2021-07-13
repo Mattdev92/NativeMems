37a475ea50a64a381f699e89b7704e96d5614657
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-07-13 15:27:14 +0200

    add Favourite route

--- screens/Favourite/Favourite.tsx
+++ screens/Favourite/Favourite.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 import { Button, Text, View } from "react-native";
 import WithNavigation from "../../templates/withNavigation/withNavigation";
-import { HotScreeenProps } from "./Hot.types";
+import { FavouriteProps } from "./Favourite.types";
 
-const HotScreen: FC<HotScreeenProps> = ({ navigation }) => (
+const HotScreen: FC<FavouriteProps> = ({ navigation }) => (
   <WithNavigation navigation={navigation}>
-    <Text>Hot MEMs</Text>
+    <Text>Favorite MEMs</Text>
     <Button title="Go back" onPress={() => navigation.goBack()} />
   </WithNavigation>
 );
